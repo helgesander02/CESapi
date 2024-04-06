b90a8cee88da907dd88054f0a154b1bf0f72698e
sander <helgesan0202@gmail.com>
2024-04-06 16:59:54 +0800

    Finish GETSource

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CESapi
 ### This is a Currency Exchange Service API that uses dependency injection and TDD design.
 
-# RUN
+## RUN
 * docker build -t 'cesapi' .
-* docker run -p 8080:8080 cesapi
+* docker run --rm -d -p 8080:8080 cesapi
+* Open the website http://localhost:8080 
--- main.go
+++ main.go
@@ -1,17 +1,77 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type SourceItem struct {
+	Source  string       `json:"source"`
+	Targets []TargetItem `json:"targets"`
+}
+
+type TargetItem struct {
+	Target string  `json:"target"`
+	FX     float64 `json:"fx"`
+}
+
+var (
+	//
+	TWDch = []TargetItem{
+		{Target: "TWD", FX: 1},
+		{Target: "JPY", FX: 3.669},
+		{Target: "USD", FX: 0.03281},
+	}
+	JPYch = []TargetItem{
+		{Target: "TWD", FX: 0.26956},
+		{Target: "JPY", FX: 1},
+		{Target: "USD", FX: 0.00885},
+	}
+	USDch = []TargetItem{
+		{Target: "TWD", FX: 30.444},
+		{Target: "JPY", FX: 111.801},
+		{Target: "USD", FX: 1},
+	}
+	//
+	TWD = SourceItem{Source: "TWD", Targets: TWDch}
+	JPY = SourceItem{Source: "JPY", Targets: JPYch}
+	USD = SourceItem{Source: "USD", Targets: USDch}
+	//
+	Currencies = []SourceItem{TWD, JPY, USD}
+)
+
+func getCurrencies(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Currencies)
+}
+
+func getSource(c *gin.Context) {
+	source := c.Query("source")
+	sourceitem, err := getItemBySource(source)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Not found"})
+	}
+
+	c.IndentedJSON(http.StatusOK, sourceitem)
+}
+
+func getItemBySource(source string) (*SourceItem, error) {
+	for _, item := range Currencies {
+		if item.Source == source {
+			return &item, nil
+		}
+	}
+
+	return nil, errors.New("todo not found")
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.GET("/", getCurrencies)
+	r.GET("/source", getSource)
+
+	r.Run()
 }
